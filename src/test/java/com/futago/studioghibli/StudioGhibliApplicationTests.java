be7f8fa8e2f6e04c2a51c8c8ca6521a3e80d0334
Ícaro Carlos Andrade Costa <icarocarlosandrade@gmail.com>
2020-04-21 08:55:47 -0300

    First commit

---
+++ src/test/java/com/futago/studioghibli/StudioGhibliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futago.studioghibli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioGhibliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

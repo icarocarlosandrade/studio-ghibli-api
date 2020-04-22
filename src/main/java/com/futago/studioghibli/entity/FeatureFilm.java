c63ed425c853aea1f80f1794922462025f902245
Ícaro Carlos Andrade Costa <icarocarlosandrade@gmail.com>
2020-04-22 14:38:25 -0300

    Cleaning entities code

--- src/main/java/com/futago/studioghibli/entity/FeatureFilm.java
+++ src/main/java/com/futago/studioghibli/entity/FeatureFilm.java
@@ -1,6 +1,5 @@
 package com.futago.studioghibli.entity;
 
-import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,43 +8,33 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
 
-
 @Entity
-@Table(name = "feature_film")
+@Table
 @Data
 public class FeatureFilm {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "feature_film_seq")
-	@SequenceGenerator(name = "feature_film_seq", sequenceName = "feature_film_id_seq", allocationSize = 1)
-	@Basic(optional = false)
-	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false, updatable = false)
 	private Integer id;
 
-	@Column(name = "year")
 	private String year;
 
-	@Column(name = "director")
 	private String director;
 
-	@Column(name = "screenwriter")
 	private String screenwriter;
 
-	@Column(name = "producer")
 	private String producer;
 
-	@Column(name = "music")
 	private String music;
 
-	@Column(name = "running_time")
 	private String runningTime;
 
-	@JoinColumn(name = "titles_id", referencedColumnName = "id")
 	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "titles_id", nullable = false)
 	private Titles titles;
 }
--- src/main/java/com/futago/studioghibli/entity/Titles.java
+++ src/main/java/com/futago/studioghibli/entity/Titles.java
@@ -1,39 +1,31 @@
 package com.futago.studioghibli.entity;
 
-import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
-@Table(name = "titles")
+@Table
 @Data
 public class Titles {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "titles_seq")
-	@SequenceGenerator(name = "titles_seq", sequenceName = "titles_id_seq", allocationSize = 1)
-	@Basic(optional = false)
-	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false, updatable = false)
 	private Integer id;
-	
-	@Basic(optional = false)
-	@Column(name = "japanese")
+
+	@Column(nullable = false)
 	private String japanese;
-	
-	@Column(name = "hepburn")
+
 	private String hepburn;
-	
-	@Column(name = "english")
+
 	private String english;
-	
-	@Column(name = "portuguese")
+
 	private String portuguese;
 
 }

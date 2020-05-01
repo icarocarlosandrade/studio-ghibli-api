4cf7586829af063755c87f2aa0506cad4403a272
Ícaro Carlos Andrade Costa <icarocarlosandrade@gmail.com>
2020-04-30 21:34:52 -0300

    Removed unnecessary @Table annotation

--- src/main/java/com/futago/studioghibli/entity/FeatureFilm.java
+++ src/main/java/com/futago/studioghibli/entity/FeatureFilm.java
@@ -8,12 +8,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
-@Table
 @Data
 public class FeatureFilm {
 
--- src/main/java/com/futago/studioghibli/entity/Titles.java
+++ src/main/java/com/futago/studioghibli/entity/Titles.java
@@ -5,12 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
-@Table
 @Data
 public class Titles {
 
